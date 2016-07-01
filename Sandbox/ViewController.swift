c2a39500c243b195d08553720771e7001c252ca7
Randy Etheredge <eman8519@gmail.com>
2016-07-01 18:05:12 -0400

    Init Commit

---
+++ Sandbox/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sandbox
+//
+//  Created by Randy Etheredge on 7/1/16.
+//  Copyright © 2016 Randy Etheredge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
